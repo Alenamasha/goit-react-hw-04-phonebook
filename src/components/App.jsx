1910f2b79ea609aaa964fbc2c9becfc240048525
Яричук Альона <alenamasha@gmail.com>
2023-03-29 11:21:26 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Message } from './Message/Message';
+import css from '../components/App.module.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const LOCAL_KEY = 'contacts';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const newContact = { id: nanoid(), name, number };
+
+    contacts.some(contacts => contacts.name === name)
+      ? alert(`${name}, This user is already in the contact list.`)
+      : setContacts(prevContacts => [newContact, ...prevContacts]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const filterContacts = () => {
+    const normalizFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.styleApp}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} changeFilter={changeFilter} />
+      {contacts.length > 0 ? (
+        <ContactList
+          contacts={filterContacts()}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <Message text="Contact list is empy." />
+      )}
     </div>
   );
 };
